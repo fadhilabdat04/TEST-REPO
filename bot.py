f5e182a78a599c6f0ab555aa2abad33c8d386bdf
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-06-29 17:49:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,10 @@ from config import (
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
+    FORCE_SUB_CHANNEL_1,
+    FORCE_SUB_GROUP_1,
+    FORCE_SUB_CHANNEL_2,
+    FORCE_SUB_GROUP_2,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -45,7 +49,7 @@ class Bot(Client):
         except Exception as a:
             self.LOGGER(__name__).warning(a)
             self.LOGGER(__name__).info(
-                "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                "Bot Berhenti. Gabung Group https://t.me/SiArabSupport untuk Bantuan"
             )
             sys.exit()
 
@@ -69,7 +73,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                    "Bot Berhenti. Gabung Group https://t.me/SiArabSuppor untuk Bantuan"
                 )
                 sys.exit()
 
@@ -93,7 +97,103 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                    "Bot Berhenti. Gabung Group https://t.me/SiArabSuppor untuk Bantuan"
+                )
+                sys.exit()
+
+        if FORCE_SUB_CHANNEL_1:
+            try:
+                info = await self.get_chat(FORCE_SUB_CHANNEL_1)
+                link = info.invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL_1)
+                    link = info.invite_link
+                self.invitelink = link
+                self.LOGGER(__name__).info(
+                    f"FORCE_SUB_CHANNEL_1 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                )
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_CHANNEL_1!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL_1}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/SiArabSupport untuk Bantuan"
+                )
+                sys.exit()
+
+        if FORCE_SUB_GROUP_1:
+            try:
+                info = await self.get_chat(FORCE_SUB_GROUP_1)
+                link = info.invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_GROUP_1)
+                    link = info.invite_link
+                self.invitelink2 = link
+                self.LOGGER(__name__).info(
+                    f"FORCE_SUB_GROUP_1 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                )
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_GROUP_1!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP_1}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/SiArabSupport untuk Bantuan"
+                )
+                sys.exit()
+
+        if FORCE_SUB_CHANNEL_2:
+            try:
+                info = await self.get_chat(FORCE_SUB_CHANNEL_2)
+                link = info.invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL_2)
+                    link = info.invite_link
+                self.invitelink = link
+                self.LOGGER(__name__).info(
+                    f"FORCE_SUB_CHANNEL_2 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                )
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_CHANNEL_2!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL_2}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/SiArabSupport untuk Bantuan"
+                )
+                sys.exit()
+
+        if FORCE_SUB_GROUP_2:
+            try:
+                info = await self.get_chat(FORCE_SUB_GROUP_2)
+                link = info.invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_GROUP_2)
+                    link = info.invite_link
+                self.invitelink2 = link
+                self.LOGGER(__name__).info(
+                    f"FORCE_SUB_GROUP_2 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                )
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_GROUP_2!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP_2}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/SiArabSupport untuk Bantuan"
                 )
                 sys.exit()
 
@@ -117,7 +217,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SiArabSupport"
         )
 
     async def stop(self, *args):
